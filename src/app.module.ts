4ba803b40efef5b17502c1f8d29173e2ae148d0b
gustavo <gustavo.limafgz@gmail.com>
2025-12-19 10:56:41 -0300

    init db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { SharedModule } from './shared/shared.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const port = Number(process.env.DATABASE_PORT);
+
+if (Number.isNaN(port)) {
+  throw new Error('DATABASE_PORT inválida');
+}
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
